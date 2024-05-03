f9925135d8a049ea124638b025162f032c9ff3b8
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-03 12:20:24 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esempio</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex justify-content-center my-4">
+            <h1>Sales Taxes Project</h1>
+        </div>
+    </div>
+</body>
+
+</html>
