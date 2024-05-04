ec654da10954b6c49a9f4df5ff108e2c5cf299f5
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-04 20:24:53 +0200

    add the products in blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esempio</title>
+    <title>Sales Taxes Project</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
     <div class="container">
-        <div class="d-flex justify-content-center my-4">
-            <h1>Sales Taxes Project</h1>
+        <h1 class="text-center my-4">Sales Taxes Project</h1>
+        <div class="row m-auto">
+            <div class="col-11 m-auto">
+                <table class="table table-striped table-hover text-center table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="col">Name</th>
+                            <th class="col-1">Price</th>
+                            <th class="col-3">Category</th>
+                            <th class="col-1">Buy</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                        <tr>
+                            <td>
+                                {{$product->name}}
+                            </td>
+                            <td>
+                                {{$product->price}}
+                            </td>
+                            <td>
+                                {{ $product->category->name }}
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-primary">Select</button>
+                            </td>
+
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
+    </div>
     </div>
 </body>
 
