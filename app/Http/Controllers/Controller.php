4b2251b54384338a4f60fd8fc24a4652fffd4a6e
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-04 20:24:26 +0200

    add method index for show the products

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Category;
+use App\Models\Product;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        //faccio vedere tutti i prodotti
+        $products = Product::all();
+        return view('index', compact('products'));
+    }
 }
