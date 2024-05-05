94e8723d8403714eea46715f68ecdc627bfeae0b
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-05 17:59:29 +0200

    create function in Controller for show cart and add products on the cart

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
 
@@ -17,5 +19,40 @@ class Controller extends BaseController
         //faccio vedere tutti i prodotti
         $products = Product::all();
         return view('index', compact('products'));
+    }
+
+    public function showCart()
+    {
+        $productsOnCart = Cart::all();
+        return view('cart', compact("productsOnCart"));
+    }
+
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+
+        /*verificare se il prodotto gia esistente nel Cart,
+         se esiste aggiungere +1 alla quantità, 
+         altrimenti crearne uno nuovo 
+         */
+
+        $cartItem = Cart::where("id_product", $product->id)->first();
+
+        if ($cartItem) {
+            $cartItem->quantity = $cartItem->quantity + 1;
+            $cartItem->price = $cartItem->price + $product->price;
+        } else {
+            $cartItem = new Cart();
+            $cartItem->id_product = $product->id;
+            $cartItem->name_product = $product->name;
+            $cartItem->quantity = 1;
+            $cartItem->price = $product->price;
+        }
+
+        $cartItem->save();
+
+        $productsOnCart = Cart::all();
+
+        return view('cart', compact("productsOnCart"));
     }
 }
