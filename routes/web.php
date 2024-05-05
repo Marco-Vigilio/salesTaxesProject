8bf1bf641d4b70ca60e43ccc714d1fe6d8acc425
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-05 19:11:30 +0200

    create route delete

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\Controller::class, 'index'])->name('index');
 Route::get('/cart', [App\Http\Controllers\Controller::class, 'showCart'])->name('cart');
 Route::post('/cart/{id}', [App\Http\Controllers\Controller::class, 'addToCart'])->name('addToCart');
+Route::delete('/cart/{id}', [App\Http\Controllers\Controller::class, 'deleteProductOnCart'])->name('deleteProductOnCart');
