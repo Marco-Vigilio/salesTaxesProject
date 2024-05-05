0460db4b8af40eb1d102a1f09e4c1e079e4dbcfe
Marco-Vigilio <tonyvigilio@gmail.com>
2024-05-05 18:00:21 +0200

    add route for function showCart/addToCart

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\Controller::class, 'index'])->name('index');
+Route::get('/cart', [App\Http\Controllers\Controller::class, 'showCart'])->name('cart');
+Route::post('/cart/{id}', [App\Http\Controllers\Controller::class, 'addToCart'])->name('addToCart');
